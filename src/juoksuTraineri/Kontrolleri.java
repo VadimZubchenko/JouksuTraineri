f4996c0432e298b5554fb1c9a4dc8679697fac07
VadimZubchenko <36922064+VadimZubchenko@users.noreply.github.com>
2018-03-12 11:22:43 +0200

    lisäsin pelto, metsä, mäki

--- src/juoksuTraineri/Kontrolleri.java
+++ src/juoksuTraineri/Kontrolleri.java
@@ -11,7 +11,6 @@ public class Kontrolleri {
     // kapseloidut ominaisuudet (V, M)
     private Naytto naytto;
     private SykeMittari mittari;
-    private int nopeus;
 
     // metodi luo tarvittavat MVC-sovelluksen komponentit ja 
     // käynnistää MVC-sovelluksen.
@@ -34,10 +33,14 @@ public class Kontrolleri {
     }
 
     public void start(int nopeus) {
-        this.nopeus = nopeus;
         // pyydetään näyttöä kysymään käyttäjältä haluttu jouksuaika
         String vastaus = naytto.kysyTieto("Kuinka kauan juostaan?");
         Double aika = Double.parseDouble(vastaus);
+        // annetaan reitin olot
+        String reittiOlo = naytto.kysyTieto("pelto: 1, metsä 2, mäkeä: 3");
+        int olo = Integer.parseInt(reittiOlo);
+        // siirretään tietoa mittarille
+        mittari.setOlo(olo);
         // while-tostorakenteella, joka simuloi kelloa
         int vaihe = 0;
         while (aika > 0) {
@@ -51,30 +54,75 @@ public class Kontrolleri {
             // saadaan takaisin haluttu vastaus
             double tulos = aika / 5;
             if (tulos % 1 == 0) {
+
                 if (nopeus == 1) {
                     int syke = mittari.getSyke();
-                    naytto.naytaViesti(" Syke on: " + syke);
+                    if (syke >= 90 && syke <= 110) {
+                        naytto.naytaViesti("Syke: " + syke + " bpm.\n"
+                                + " Hyvin menee!");
+                    }
+                    if (syke > 110) {
+                        naytto.naytaViesti("Huomio! \n"
+                                + "Syke: " + syke + " bpm. \n"
+                                + "Korkea! \n"
+                                + "Laske nopeus! ");
+                    }
+                    if (syke < 90) {
+                        naytto.naytaViesti("Syke: " + syke + " bpm. \n"
+                                + " Liian pieni. \n"
+                                + "Kävele nopeampi! ");
+                    }
                 }
                 if (nopeus == 2) {
                     int syke = mittari.getSyke1();
-                    naytto.naytaViesti(" Syke on: " + syke);
+                    if (syke >= 140 && syke <= 160) {
+                        naytto.naytaViesti("Syke: " + syke + " bpm. \n"
+                                + "Hyvin menee!");
+                    }
+                    if (syke > 160) {
+                        naytto.naytaViesti("Huomio! \n"
+                                + " Syke: " + syke + " bpm. \n"
+                                + "Korkea! \n"
+                                + "Laske nopeus! ");
+                    }
+                    if (syke < 140) {
+                        naytto.naytaViesti("Syke: " + syke + " bpm. \n"
+                                + "Liian pieni. \n"
+                                + "Juokse nopeampi! ");
+                    }
                 }
                 if (nopeus == 3) {
                     int syke = mittari.getSyke2();
-                    naytto.naytaViesti(" Syke on: " + syke);
+                    if (syke >= 150 && syke <= 180) {
+                        naytto.naytaViesti("Syke: " + syke + " bpm.  \n"
+                                + "Hyvin menee!");
+                    }
+                    if (syke > 180) {
+                        naytto.naytaViesti("Huomio! \n"
+                                + "Syke: " + syke + " bpm. \n"
+                                + "Korkea! \n"
+                                + "Laske nopeus! ");
+                    }
+                    if (syke < 150) {
+                        naytto.naytaViesti("Syke: \n" + syke + " bpm. \n"
+                                + "Liian pieni. \n"
+                                + "Juokse nopeampi! ");
+                    }
+
                 }
 
             }
         }
 
-        naytto.naytaViesti("Onneksi olkoon! Olet suorittanut juoksuohjelman!");
+        naytto.naytaViesti(
+                "Onneksi olkoon! Olet suorittanut juoksuohjelman!");
 
         // siirrytään takaisin aloitusnäyttöön
         naytto.aloitusNaytto();
 
     }
-   
-    // Koko sovelluksen käynnistäminen
+
+// Koko sovelluksen käynnistäminen
     public static void main(String[] args) {
         // luodaan uusi kontrolleri-olio, ja
         // kutsutaan samalla sovelluksen käynnistää metodia.
